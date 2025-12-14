4a3e5cca9b355cbab9270aecb9e32d40fa49c698
Lenny <AnasIssaThe@gmail.com>
2025-12-14 18:56:53 -0500

    Add Department class with associated helper methods

---
+++ src/main/java/org/example/Department.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@EqualsAndHashCode
+@ToString
+@Getter
+public class Department {
+    private String departmentId;
+    @Setter
+    private String departmentName;
+
+    private static int nextId = 0;
+
+    public Department(String departmentName) {
+        if (isDepartmentNameValid(departmentName)) {
+            this.departmentId = String.format("%d", nextId++);
+            this.departmentName = departmentName;
+        } else {
+            this.departmentId = null;
+            this.departmentName = null;
+        }
+    }
+
+    /**
+     * Checks if a department name is valid or not
+     * A department name should only contain letters or spaces
+     * @param departmentName The department name to be checked
+     * @return True if the name is valid, otherwise false
+     */
+    static boolean isDepartmentNameValid(String departmentName) {
+        if (departmentName == null) {
+            return false;
+        }
+
+        for (char c : departmentName.toCharArray()) {
+            if (!Character.isDigit(c) && c != ' ') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
